97481bffbba6f72aae5532af379c9b40d7bdac4c
SDV Bordeaux <sdvbordeaux@MBPBX0018.local>
2024-03-05 11:27:48 +0100

    Basic is now working

--- PlayerGathering.gg/Model/RunesModel.swift
+++ PlayerGathering.gg/Model/RunesModel.swift
@@ -1,16 +1,40 @@
+import Foundation
+
 class RunesModel: ObservableObject {
-
+    
     @Published var perks = [Perks] ()
-
+    
+    func primaryRune(for player: Info.Participants?) -> (Int, Int) {
+        guard let player = player else { return (0, 0) }
+        
+        guard let primaryPerk = player.perks.styles.first?.style else { return (0, 0) }
+        
+        // Assuming player.perks is of type Info.Participants.Perks
+        guard let primaryStyle = player.perks.styles.first?.selections.first?.perk else { return (0, 0) }
+        
+        // Assuming Perks.Styles has a description property
+        return (primaryPerk, primaryStyle)
+    }
+    
+    func secondaryRune(for player: Info.Participants?) -> Int {
+        guard let player = player else { return 0 }
+        
+        // Assuming player.perks is of type Info.Participants.Perks
+        let secondaryStyle = player.perks.styles[1].style
+        
+        // Assuming Perks.Styles has a description property
+        return secondaryStyle
+    }
+    
     func fetchData() async {
-        guard let url = "https://ddragon.leagueoflegends.com/cdn/10.16.1/data/en_US/runesReforged.json" else {
+        guard let url = URL(string: "https://ddragon.leagueoflegends.com/cdn/10.16.1/data/en_US/runesReforged.json") else {
             print("URL Invalide")
             return
         }
-
+        
         do {
             let (data, response) = try await URLSession.shared.data(from: url)
-
+            
             if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 {
                 do {
                     let decodeResponse = try JSONDecoder().decode([Perks].self, from: data)
@@ -29,5 +53,30 @@ class RunesModel: ObservableObject {
         catch {
             print("Network Request error: \(error)")
         }
+    }
+    func fetchSecondaryRuneIcon(for runeID: Int) -> String? {
+        // Search for the rune in the perks array
+        for perk in perks {
+            if(perk.id == runeID) {
+                return perk.icon
+            }
+        }
+        return nil // Return nil if rune with given ID is not found
+    }
+    func fetchPrimaryRuneIcon(for runeID: Int, for perkID: Int) -> String? {
+        // Search for the rune in the perks array
+        for perk in perks {
+            print(perk.id)
+            if(perk.id == perkID) {
+                for slot in perk.slots {
+                    for rune in slot.runes {
+                        if rune.id == runeID {
+                            return rune.icon
+                        }
+                    }
+                }
+            }
+        }
+        return nil // Return nil if rune with given ID is not found
     }
 }
